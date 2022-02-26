833775f58f46d90b6252f63ef03c0d99b0e8f3ce
Sara Samer <ss.titanic199@gmail.com>
2022-02-26 10:39:42 +0200

    maven init witrh jobs csv

--- README.md
+++
@@ -1,2 +0,0 @@
-# wuzzuf-jobs-analysis
-A RESTful API for modeling and analyzing Kaggle Wuzzuf jobs 
---
+++ src/main/java/com/example/WuzzufJobsAnalysis/WuzzufJobsAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.WuzzufJobsAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuzzufJobsAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuzzufJobsAnalysisApplication.class, args);
+	}
+
+}
