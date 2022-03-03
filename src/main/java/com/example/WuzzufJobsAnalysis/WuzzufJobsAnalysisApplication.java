cce1a404d630f06792cd45d41e949f7963f98573
Sara Samer <ss.titanic199@gmail.com>
2022-03-03 20:40:31 +0200

    Added spark dependencies and log4j properties

--- src/main/java/com/example/WuzzufJobsAnalysis/WuzzufJobsAnalysisApplication.java
+++ src/main/java/com/example/WuzzufJobsAnalysis/WuzzufJobsAnalysisApplication.java
@@ -1,5 +1,6 @@
 package com.example.WuzzufJobsAnalysis;
 
+import org.apache.spark.sql.SparkSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WuzzufJobsAnalysisApplication {
 
 	public static void main(String[] args) {
+		SparkSession spark = SparkSession
+				.builder()
+				.appName("Java Spark ML project")
+				.master("local[4]")
+				.config("spark.master", "local")
+				.getOrCreate();
 		SpringApplication.run(WuzzufJobsAnalysisApplication.class, args);
 	}
 
