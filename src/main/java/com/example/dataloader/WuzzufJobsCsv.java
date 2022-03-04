952e8db82e8d78c0beb522020a2cd1da81ea8083
Sara Samer <ss.titanic199@gmail.com>
2022-03-04 23:49:55 +0200

    read wuzzuf csv file

--- src/main/java/com/example/dataloader/WuzzufJobsCsv.java
+++ src/main/java/com/example/dataloader/WuzzufJobsCsv.java
@@ -11,26 +11,17 @@ import org.apache.spark.sql.SparkSession;
  */
 public class WuzzufJobsCsv implements DataLoaderDAO {
 
+    public WuzzufJobsCsv() {
+        super();
+    }
+
     public Dataset<Row> load(String filename) {
-        SparkSession spark = SparkSession.builder()
-                .getOrCreate();
-        // spark.sparkContext().setLogLevel("ERROR");
+        SparkSession spark = SparkSession.builder().getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
         Dataset<Row> dataset = spark.read()
                 .option("header", "true")
                 .csv(filename);
-        dataset.show(5);
-        System.out.println("+++++========++++++++Done");
-        // dataset.createOrReplaceTempView("WUZZUF_DATA");
-        // Dataset<Row> typedDataset = spark.sql("SELECT "
-        //         + "cast (Title as string) Title, "
-        //         + "cast (Company as string) Company, "
-        //         + "cast (Location as string) Location, "
-        //         + "cast (Type as string) Type, "
-        //         + "cast (Level as string) Level, "
-        //         + "cast (YearsExp as string) YearsExp, "
-        //         + "cast (Country as string) Country, "
-        //         + "cast (Skills as string) Skills FROM WUZZUF_DATA");
-        // return typedDataset;
-        return null;
+
+        return dataset;
     }
 }
