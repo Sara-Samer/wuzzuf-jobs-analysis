20ab3be78bba7b4aa63f34c0323554e309f2419d
Sara Samer <ss.titanic199@gmail.com>
2022-03-04 23:49:32 +0200

    made user defined functions to factorize years exp column to two columns

---
+++ src/main/java/com/example/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.example.utils;
+
+public class Constants {
+    public static final String COLUMN_PARSE_YEARS_OF_EXP_UDF_NAME = "columnParseYearsOfExpUdf";
+    public static final String COLUMN_PARSE_MIN_UDF_NAME = "columnParseMinUdf";
+    public static final String COLUMN_PARSE_MAX_UDF_NAME = "columnParseMaxUdf";
+    public static final String PARSED_COLUMN_NAME = "MinMaxYearsExp";
+    public static final String MIN_COLUMN_NAME = "MinYearsExp";
+    public static final String MAX_COLUMN_NAME = "MaxYearsExp";
+}
